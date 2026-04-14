6d6e09c50fcc43b79b1a1aa459d9b5d41e7cffb5
a-anuj <anujamirthalingam5@gmail.com>
2026-04-14 15:05:08 +0530

    fix : skip to os.walk() for vague queries

--- cli/main.py
+++ cli/main.py
@@ -1,6 +1,6 @@
 import sys
 from core.parser import parse_query
-from core.search import search_files
+from core.search import fast_search, search_files
 
 
 def main():
@@ -9,9 +9,10 @@ def main():
         return
 
     query = " ".join(sys.argv[1:])
-
     parsed = parse_query(query)
-    results = search_files(parsed)
+    if not query:
+        return search_files(parsed)  # your old method
+    results = fast_search(parsed)
 
     if not results:
         print("No files found")
